76b4551fea04922b948e2243d019046e8165461b
Ananya Nair <an.nair1702@gmail.com>
2023-01-21 18:40:47 +0530

    Initial structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from "react";
+import Axios from "axios";
+
+
 
 function App() {
+
+const getNews = () => {
+
+  Axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=f7f792eeba474a14bcfac3bf0e293cec")
+  .then((response)=>{
+    console.log(response);
+  })
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className="title"> TopNews </h1>
+      <button className='button' onClick = {getNews}> Get News </button>
     </div>
   );
 }
