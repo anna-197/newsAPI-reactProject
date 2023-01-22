6c90085fed16b3f0f059467bcd6b95df1bfcfcf7
Ananya Nair <an.nair1702@gmail.com>
2023-01-22 13:47:24 +0530

    updated CSS

--- src/App.js
+++ src/App.js
@@ -1,50 +1,54 @@
-import './App.css'
+import "./App.css";
 import React, { useState } from "react";
 import Axios from "axios";
 
-
-
 function App() {
+  const [news, setNews] = useState([]);
 
-  const[ news, setNews ] = useState([]);
-
-const getNews = () => {
-
-  Axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=f7f792eeba474a14bcfac3bf0e293cec")
-  .then((response)=>{
-    console.log(response);
-    setNews(response.data.articles);
-  })
-
-}
+  const getNews = () => {
+    Axios.get(
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=f7f792eeba474a14bcfac3bf0e293cec"
+    ).then((response) => {
+      console.log(response);
+      setNews(response.data.articles);
+    });
+  };
 
   return (
     <>
+      <div className="container">
+        <h1 className="title"> TopNews </h1>
+        <button className="button" onClick={getNews}>
+          Get News
+        </button>
+      </div>
 
-    <div className='container'>
-      <h1 className="title"> TopNews </h1>
-      <button className='button' onClick = {getNews}> Get News </button>
-    </div>
+      <div className="news-container">
+        {news.map((index) => {
+          return (
+            <>
+              <div id="container">
+                <div class="news-details">
+                  <h1>{index.title}</h1>
 
-    <div className='news-container'>
-      {
-        news.map((index)=>{
-          return(
-           <>
-           <img src={index.urlToImage} alt='default' />
+                  <p class="information">{index.description}</p>
 
-          <h5>{index.title}</h5>
-           </>
-          )
-        })
-      }
-    </div>
+                  <div class="control">
+                    <button class="btn">
+                      <a href={index.url} class="readmore">Read More</a>
+                    </button>
+                  </div>
+                </div>
 
-
-
+                <div class="news-image">
+                  <img src={index.urlToImage} alt="default" />
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </>
-    
-
   );
 }
 
